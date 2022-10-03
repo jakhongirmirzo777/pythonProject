415ca24e113edbf859e4a4356bb84456dedcd80f
Jakhongirmirzo Tursunaliev <00013540@wiut.uz>
2022-10-03 10:20:56 +0500

    Finish

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-def encode (message):
-    encoded_message = ""
-    i = 0
-    while(i <= len(message) - 1):
-        count = 1
-        char = message[i]
-        j=i
-        while(j < len(message) - 1):
-            if (message[j] == message[j + 1]):
-                count = count+ 1
-                j = j + 1
-            else:
-                break
-        encoded_message = encoded_message + str(count) + char
-        i = j + 1
-    return encoded_message
+# def encode (message):
+#     encoded_message = ""
+#     i = 0
+#     while(i <= len(message) - 1):
+#         count = 1
+#         char = message[i]
+#         j=i
+#         while(j < len(message) - 1):
+#             if (message[j] == message[j + 1]):
+#                 count = count+ 1
+#                 j = j + 1
+#             else:
+#                 break
+#         encoded_message = encoded_message + str(count) + char
+#         i = j + 1
+#     return encoded_message
+#
+#
+# encoded_message = encode("ABBBBCCCCCCCCAB")
+# print(encoded_message)
 
-
-encoded_message = encode("ABBBBCCCCCCCCAB")
-print(encoded_message)
+def callModule ():
+    moduleName = input('Write your module name')
+    numberOfComponents = input('Write number of components')
